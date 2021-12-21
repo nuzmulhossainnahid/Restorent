14eab163332c72dbe77247b9412bd0d4f5584136
nuzmulhossainnahid <hasannahidnazmul@gmail.com>
2021-12-21 13:09:19 +0600

    1

---
+++ resources/views/front/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Restoran - Bootstrap Restaurant Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    @include('front.includes.style')
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+@yield('body')
+<div class="container-xxl bg-white p-0">
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+@include('front.includes.header')
+@include('front.includes.footer')
+@include('front.includes.script')
+
+@yield('body')
+
+
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+</div>
+
+
+</body>
+
+</html>
